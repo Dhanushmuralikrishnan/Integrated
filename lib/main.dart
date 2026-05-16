3cc915ad8d2604512075d2fdc1ad7059fa7f8955
Dhanushmuralikrishnan <155611892+Dhanushmuralikrishnan@users.noreply.github.com>
2026-05-16 18:37:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class EnergyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         brightness: Brightness.light,
         useMaterial3: true,
-        cardTheme: CardThemeData(
+        cardTheme: CardTheme(
             elevation: 4,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
